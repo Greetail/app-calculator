653ad616538c9e61f86fc83406ff6bebaf3e752a
Jose M. Delgado <jdelgado@greetail.co>
2017-04-13 08:24:11 -0500

    getting data from firebase using vuefire

--- app.js
+++ app.js
@@ -1,39 +1,22 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAobWUy0Pr2mOsfkT2zGplFVp1IauzVKJQ",
+    authDomain: "greetail-calculator.firebaseapp.com",
+    databaseURL: "https://greetail-calculator.firebaseio.com",
+    projectId: "greetail-calculator",
+    storageBucket: "greetail-calculator.appspot.com",
+    messagingSenderId: "714879421168"
+};
+
+firebase.initializeApp(config);
+
+var sectionsRef = firebase.database().ref('sections');
+
+
 //vuejs section
 var app = new Vue({
     el: '#app',
-    data: {
-        "sections": [
-            {
-                "section_name": "How do users authenticate",
-                "features": [
-                    {
-                        "name": "Email login", "description": "connect your users via email and password",
-                        "selected": false
-                    },
-                    {
-                        "name": "Social login", "description": "authorize users with social networks such as facebook or twitter",
-                        "selected": false
-                    }]
-            },
-            {
-                "section_name": "Communication",
-                "features": [
-                    {
-                        "name": "SMS - text messages",
-                        "description": "connect to your users by texting them",
-                        "selected": true
-                    },
-                    {
-                        "name": "Burrito sabanero",
-                        "description": "another burrito instance",
-                        "selected": false
-                    }]
-            }
-        ]
-    },
-    methods: {
-        reverseMessage: function () {
-            this.message = this.message.split("").reverse().join("")
-        }
+    firebase: {
+        sections: sectionsRef
     }
 });
