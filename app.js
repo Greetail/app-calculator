42678a47106f61fc86d5dffd5d4dcf7cf2773f4b
Jose M. Delgado <jdelgado@greetail.co>
2017-05-26 19:15:00 -0500

    adding header

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ var config = {
     messagingSenderId: "714879421168"
 };
 
+
+$(function() {
+    
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  
+});
+
 firebase.initializeApp(config);
 
 var sectionsRef = firebase.database().ref('sections');
